adb2a91fe4255ed5747f845dd643b66fab7a7c63
anish1A1 <alloozpat87@gmail.com>
2025-12-07 18:01:02 +0545

    added pydanticOutputParser for data validation and json format

--- 4_ConditionalChain.py
+++ 4_ConditionalChain.py
@@ -1,8 +1,9 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableParallel
-
+from pydantic import BaseModel, Field
+from typing import Literal 
 
 # We will get customer feedback add it to model, then if the response is postive or negative it will be sent to another models and response will be generated to that user.
 
@@ -20,3 +21,19 @@ llm2 = HuggingFaceEndpoint(
     task='text-generation'
 )
 model2 = ChatHuggingFace(llm=llm2)
+
+
+class feedback(BaseModel):
+    sentiment:Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+parser = PydanticOutputParser(pydantic_object=feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into positive or negative \n {feedback} \n{format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+classifier_chain = prompt1 | model | parser
+
+print(classifier_chain.invoke({'feedback': 'This smartphone is really fast! But I will not buy again '}))
