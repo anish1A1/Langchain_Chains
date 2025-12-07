6de34ed5ad9fcdd8394545a254d3edf94680254b
anish1A1 <alloozpat87@gmail.com>
2025-12-07 16:13:14 +0545

    creating a simple chain

---
+++ simpleChains.py
@@ -0,0 +1,22 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
+
+llm = HuggingFaceEndpoint(
+    repo_id= "moonshotai/Kimi-K2-Thinking",
+    task="text-generation"
+)
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+chain = prompt | model | parser
+# print(chain)
+
+result = chain.invoke({'topic':'Pokemon'})
+print(result)
